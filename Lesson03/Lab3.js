72c0fd318b75b24656fc341831d4a9caf6124c89
Ngô Quang Trường <truong@Tc.local>
2025-11-07 15:07:43 +0700

    lab3

---
+++ Lesson03/Lab3.js
@@ -0,0 +1,16 @@
+//Bài 1
+// Function 1
+const multiply = (a,b)=> a*b;
+  
+// Function 2
+const isPositive = (number)=> number >=0;
+
+// Function 3
+const getRandomNumber = () => Math.random();
+
+// Function 4
+document.addEventListener('click', ()=>{
+    console.log("Clicked");
+});
+
+//Bài2
