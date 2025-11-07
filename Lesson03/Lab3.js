2378533558106a11c55735b509643aba82ad3235
Ngô Quang Trường <truong@Tc.local>
2025-11-07 16:08:36 +0700

    update

--- Lesson03/Lab3.js
+++ Lesson03/Lab3.js
@@ -1,17 +1,17 @@
 // // //Bài 1
-// // Function 1
-// const multiply = (a,b)=> a*b;
+// Function 1
+const multiply = (a,b)=> a*b;
   
-// // Function 2
-// const isPositive = (number)=> number >=0;
+// Function 2
+const isPositive = (number)=> number >=0;
 
-// // Function 3
-// const getRandomNumber = () => Math.random();
+// Function 3
+const getRandomNumber = () => Math.random();
 
-// // Function 4
-// document.addEventListener('click', ()=>{
-//     console.log("Clicked");
-// });
+// Function 4
+document.addEventListener('click', ()=>{
+    console.log("Clicked");
+});
 
 //Bài2
 const createUser = (name="Anonymous", age=18, isAdmin=false)=>({
