7f2a4492e785d5415a99d48f88704aa2f1dda49a
Ngô Quang Trường <truong@Tc.local>
2025-11-28 14:55:23 +0700

    lab6

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -19,13 +19,12 @@ function AddPage() {
   });
 
   const handleChange = (e) => {
-    const { id, value, type, checked } = e.target;
+    const { name, value, type, checked } = e.target;
 
-    if (type === "checkbox") {
-      setForm({ ...form, active: checked });
-    } else {
-      setForm({ ...form, [id]: value });
-    }
+    setForm({
+      ...form,
+      [name]: type === "checkbox" ? checked : value,
+    });
   };
 
   const handleSubmit = async (e) => {
@@ -36,12 +35,10 @@ function AddPage() {
         price: Number(form.price),
         available: Number(form.available),
       });
-
       toast.success("Thêm Tour thành công!");
       navigate("/list");
     } catch (error) {
-      toast.error("Lỗi khi thêm dữ liệu!");
-      console.log(error);
+      toast.error(error.message);
     }
   };
 
@@ -50,82 +47,90 @@ function AddPage() {
       <h1 className="text-2xl font-semibold mb-6">Thêm mới</h1>
 
       <form className="space-y-4" onSubmit={handleSubmit}>
+        
         <div>
           <label className="block font-medium mb-1">Tên Tour</label>
           <input
             type="text"
-            id="name"
+            name="name"
             value={form.name}
             onChange={handleChange}
             className="w-full border rounded-lg px-3 py-2"
           />
         </div>
+
         <div>
           <label className="block font-medium mb-1">Điểm đến</label>
           <input
             type="text"
-            id="destination"
+            name="destination"
             value={form.destination}
             onChange={handleChange}
             className="w-full border rounded-lg px-3 py-2"
           />
         </div>
+
         <div>
           <label className="block font-medium mb-1">Thời gian Tour</label>
           <input
             type="text"
-            id="duration"
+            name="duration"
             value={form.duration}
             onChange={handleChange}
             className="w-full border rounded-lg px-3 py-2"
           />
         </div>
+
         <div>
           <label className="block font-medium mb-1">Giá Tour</label>
           <input
             type="number"
-            id="price"
+            name="price"
             value={form.price}
             onChange={handleChange}
             className="w-full border rounded-lg px-3 py-2"
           />
         </div>
+
         <div>
           <label className="block font-medium mb-1">Ảnh</label>
           <input
             type="text"
-            id="image"
+            name="image"
             value={form.image}
             onChange={handleChange}
             className="w-full border rounded-lg px-3 py-2"
           />
         </div>
+
         <div>
           <label className="block font-medium mb-1">Mô tả</label>
           <input
             type="text"
-            id="description"
+            name="description"
             value={form.description}
             onChange={handleChange}
             className="w-full border rounded-lg px-3 py-2"
           />
         </div>
+
         <div>
           <label className="block font-medium mb-1">Số lượng</label>
           <input
             type="number"
-            id="available"
+            name="available"
             value={form.available}
             onChange={handleChange}
             className="w-full border rounded-lg px-3 py-2"
           />
         </div>
+
         <div>
           <label className="block font-medium mb-1">Trạng thái</label>
           <div className="flex items-center space-x-2">
             <input
               type="checkbox"
-              id="active"
+              name="active"
               checked={form.active}
               onChange={handleChange}
               className="h-4 w-4"
@@ -133,10 +138,11 @@ function AddPage() {
             <span>{form.active ? "Hoạt động" : "Không hoạt động"}</span>
           </div>
         </div>
+
         <div>
           <label className="block font-medium mb-1">Danh mục</label>
           <select
-            id="category"
+            name="category"
             value={form.category}
             onChange={handleChange}
             className="w-full border rounded-lg px-3 py-2"
@@ -145,6 +151,7 @@ function AddPage() {
             <option value="2">Tour quốc tế</option>
           </select>
         </div>
+
         <button
           type="submit"
           className="px-5 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700"
